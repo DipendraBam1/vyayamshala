2cbd4dcabd41729c8b3ad25fa19aa31753a674ac
DipendraBam1 <dipendra1431234@gmail.com>
2026-09-22 01:57:47 +0545

    fix api start deploy

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,6 +1,11 @@
-import { env } from "./config/env.js";
 import app from "./app.js";
 
-app.listen(env.PORT, () => {
-  console.log(`API running on http://localhost:${env.PORT}`);
-});
+const PORT = Number(process.env.PORT) || 5000;
+
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`API running on http://localhost:${PORT}`);
+  });
+}
+
+export default app;
