52d77841b7164739711cf7f9f583fb183d9742d8
DipendraBam1 <dipendra1431234@gmail.com>
2026-09-22 03:21:14 +0545

    connect frontend to deployed api

--- apps/web/src/services/api.ts
+++ apps/web/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://vyayamshala-api.vercel.app/api",
 });
 
 api.interceptors.request.use((config) => {
